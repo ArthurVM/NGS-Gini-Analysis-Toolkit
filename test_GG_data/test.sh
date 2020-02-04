f11e73bd21a93ff40a771c0f7a8b74955b3954a2
ArthurVM <arthurvmorris@gmail.com>
2020-02-04 10:11:03 -0800

    first commit

---
+++ test_GG_data/test.sh
@@ -0,0 +1,14 @@
+root="/home/amorris/BioInf/software/Read-distribution-analysis"
+
+gini="python $root/src/gini.py"
+test_dir="$root/test_data"
+
+lb=1
+ub=5002
+s=5
+
+for cov in $test_dir/*; do
+	fbname=$(basename "$cov" .cov)
+	echo $fbname".GG"
+	$gini $cov -G $lb,$ub,$s > $root/$fbname".GG"
+done
