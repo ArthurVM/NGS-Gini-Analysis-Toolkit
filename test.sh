26baff4dc2c65780a0f49fabe84a9098cf5e51d1
ArthurVM <arthurvmorris@gmail.com>
2020-02-06 06:16:05 -0800

    expanding naiive pipeline test

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
-root="/home/amorris/BioInf/software/Read-distribution-analysis"
+root="."
 
 gini="python3 $root/src/gini.py"
+g_merge="python3 $root/src/GG_out_merge.py"
+gg_curve="python3 $root/src/GG_curve.py"
+
 test_dir="$root/test_data"
 
 lb=1
@@ -12,3 +15,9 @@ for cov in $test_dir/*; do
 	echo $fbname".GG"
 	$gini $cov -G $lb $ub $s > $root/$fbname".GG"
 done
+
+$g_merge --Gini_file $root/*.GG -o test_g_merged.GG
+
+$gg_curve $root/test_g_merged.GG
+
+echo "All Done"
